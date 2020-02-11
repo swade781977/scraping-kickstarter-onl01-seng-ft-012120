7eda581cf603b71d83fdb3714b09ee7d8b004500
Chris Wade <swade781977@yahoo.com>
2020-02-11 19:03:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
   # porjects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project_thumbnail a img").attribute("src").value
-  
+  # description: project.css("p.bbcard_blurb").text
 end
 
 create_project_hash
