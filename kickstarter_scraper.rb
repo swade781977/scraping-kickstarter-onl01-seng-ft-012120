5cf1bd77bfb0422ed221674639cf11e7590ac7ff
Chris Wade <swade781977@yahoo.com>
2020-02-11 19:24:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,8 @@ def create_project_hash
   
   kickstarter.css("li.project.grid_4").each do |project|
     title =project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = { :image_link => project.css("div.project_thumbnail a img").attribute("src").value, 
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project_thumbnail a img").attribute("src").value, 
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name"),
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
