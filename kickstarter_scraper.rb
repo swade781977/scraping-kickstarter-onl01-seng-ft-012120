8fccc8cae42da6bb5a033778d8e0278a4b229dc2
Chris Wade <swade781977@yahoo.com>
2020-02-11 18:42:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,5 +2,8 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
+  
 end
